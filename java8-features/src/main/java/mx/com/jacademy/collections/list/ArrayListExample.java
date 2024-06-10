732d04dea0e2c85c93a5ea1295518029dcb00e2b
jonathan0109 <jonathan.martinez0609@gmail.com>
2024-06-10 01:21:14 -0600

    ArrayList main methods

--- java8-features/src/main/java/mx/com/jacademy/collections/list/ArrayListExample.java
+++ java8-features/src/main/java/mx/com/jacademy/collections/list/ArrayListExample.java
@@ -1,8 +1,9 @@
 package mx.com.jacademy.collections.list;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public class ArrayListExample {
+public class ArrayListExample{
 
     public static void main(String[] args){
         
@@ -19,17 +20,55 @@ public class ArrayListExample {
         System.out.println(arrayList); //[100, welcome, 15.5, A, true]
 
         // Size
-        System.out.println("Number of elements in array list:"+arrayList.size());
+        System.out.println("\nNumber of elements in array list:"+arrayList.size());
 
-        // Remove 'welcome' element
+        // Remove 'welcome' element by index
         arrayList.remove(1);
-        //arrayList.remove("welcome")
-        //[100, 15.5, A, true]
-        System.out.println("After removing 'welcome' element from array list:"+arrayList);
+        // Remove 'welcome' element by word match
+        // arrayList.remove("welcome")
+        // [100, 15.5, A, true]
+        System.out.println("\nAfter removing 'welcome' element from array list:"+arrayList);
 
         // Insert new element
-        //[100, 15.5, Python, A, true]
+        // [100, 15.5, Python, A, true]
         arrayList.add(2, "Python");
-        System.out.println("After insertion on second position:"+arrayList);
+        System.out.println("\nAfter insertion on second position:"+arrayList);
+
+        //Retrieve specific element
+        //Python
+        System.out.println("\nGetting second position from list:"+arrayList.get(2));
+
+        // Change/replace element
+        // [100, 15.5, C#, A, true]
+        arrayList.set(2, "C#");
+        System.out.println("\nAfter replacing element:"+arrayList);
+
+        // Using 'contains' for searching on list - Returns true/false
+        System.out.println("\nIs C# on list?:"+arrayList.contains("C#"));
+        System.out.println("Is C++ on list?:"+arrayList.contains("C++"));
+
+        // Is empty
+        System.out.println("\nIs list empty?:"+arrayList.isEmpty());
+
+        // For loop
+        System.out.println("\nReading elements using for loop");
+        for(int i=0; i<arrayList.size(); i++){
+            System.out.println(arrayList.get(i));
+        }
+
+        // For each loop
+        System.out.println("\nReading elements using for each loop");
+        for(Object object:arrayList){
+            System.out.println(object);
+        }
+
+        // Iterator
+        // You might need to use iterators if you need to modify collection in your loop
+        System.out.println("\nReading elements using iterator method");
+        Iterator iterator = arrayList.iterator();
+        
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
     }
 }

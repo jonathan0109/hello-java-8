eacbe8eb9706728d0fab7066150a6fa76dd3c0fe
jonathan0109 <jonathan.martinez0609@gmail.com>
2024-06-13 16:43:32 -0600

    Lambda Expressions - Functional Interfaces

---
+++ java8-features/src/main/java/mx/com/jacademy/lambdas/Test.java
@@ -0,0 +1,27 @@
+package mx.com.jacademy.lambdas;
+
+/**
+ * A functional interface is an interface that contains only one abstract method.
+ *  They can have only one functionality to exhibit.
+ */
+@FunctionalInterface
+interface Cab {
+
+    String bookCab(String source, String destination);
+}
+
+public class Test {
+    
+    public static void main(String[] args) {
+
+        /**
+         * Lambda expressions can be used to represent the instance of a functional interface.
+         */
+        Cab cab = (source, destination) -> {
+            System.out.println("Ola cab is booked from " + source + " to " + destination);
+            return ("Price : 5000 Rs");
+        };
+
+        System.out.println(cab.bookCab("Mexico", "USA"));
+    }
+}
